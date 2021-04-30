f5dfafae2f68bf6633e33f336961178b26d39f8f
Aakash Banerjee <aakash.banerjee@gmail.com>
2021-04-29 23:53:02 -0700

    stack #1

--- data/data.go
+++ data/data.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/godatastruct/stack"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var s stack.Stack
+	s.Push(1)
+	printStack(s.Display())
+	s.Push(2)
+	printStack(s.Display())
+	s.Push(3)
+	printStack(s.Display())
+	s.Pop()
+	printStack(s.Display())
+	fmt.Println("Is Stack Empty: ", s.IsEmpty())
+}
+
+func printStack(st []int, t int) {
+	fmt.Printf("Stack Elements: %v\t \n", st)
+	fmt.Printf("Stack Top: %v\n", t-1)
 }
--- data/go.mod
+++ data/go.mod
@@ -1,3 +1,7 @@
 module github.com/godatastruct/data
 
 go 1.15
+
+replace github.com/godatastruct/stack => ../stack
+
+require github.com/godatastruct/stack v0.0.0-00010101000000-000000000000
