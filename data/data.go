09d2022f789e1cd5a33165e97f9ba7ce9916e1d2
Aakash Banerjee <aakash.banerjee@gmail.com>
2021-04-30 12:27:35 -0700

    implemented interface {}

--- data/data.go
+++ data/data.go
@@ -7,19 +7,42 @@ import (
 )
 
 func main() {
-	var s stack.Stack
-	s.Push(1)
+	s := &stack.Stack{}
+	fmt.Printf("Type of variable s: %T\n", s)
+	s.Push("aakash")
 	printStack(s.Display())
 	s.Push(2)
 	printStack(s.Display())
-	s.Push(3)
+	s.Push(3.14)
 	printStack(s.Display())
-	s.Pop()
+	d := s.Pop()
+	if d != nil {
+		fmt.Printf("Popped: %v\n", d)
+	}
+
+	printStack(s.Display())
+	fmt.Println("Is Stack Empty: ", s.IsEmpty())
+	d = s.Pop()
+	if d != nil {
+		fmt.Printf("Popped: %v\n", d)
+	}
+	printStack(s.Display())
+	fmt.Println("Is Stack Empty: ", s.IsEmpty())
+	d = s.Pop()
+	if d != nil {
+		fmt.Printf("Popped: %v\n", d)
+	}
+	printStack(s.Display())
+	fmt.Println("Is Stack Empty: ", s.IsEmpty())
+	d = s.Pop()
+	if d != nil {
+		fmt.Printf("Popped: %v\n", d)
+	}
 	printStack(s.Display())
 	fmt.Println("Is Stack Empty: ", s.IsEmpty())
 }
 
-func printStack(st []int, t int) {
+func printStack(st []interface{}, t int) {
 	fmt.Printf("Stack Elements: %v\t \n", st)
-	fmt.Printf("Stack Top: %v\n", t-1)
+	//fmt.Printf("Stack Top: %v\n", t)
 }
--- data/stack/stack.go
+++ data/stack/stack.go
@@ -1,31 +1,40 @@
 package stack
 
+import "fmt"
+
 //Stack implements a LIFO data structure
 type Stack struct {
-	i   []int
+	i   []interface{} //could be of any type
 	top int
 }
 
 //Push inserts an element to the stack
-func (s *Stack) Push(i int) {
-	s.i = append(s.i, i)
-	s.top++
+func (s *Stack) Push(d interface{}) {
+	s.i = append(s.i, d)
+	s.top = len(s.i)
 }
 
 //Display Stack
-func (s *Stack) Display() ([]int, int) {
+func (s *Stack) Display() ([]interface{}, int) {
 	return s.i, s.top
 }
 
-//Pop removes the last inserted element from thee stack
-func (s *Stack) Pop() {
-	s.top--
-	s.i = s.i[:s.top]
+//Pop removes the last inserted element from the stack
+func (s *Stack) Pop() interface{} {
+	if !s.IsEmpty() {
+		data := s.i[s.top-1]
+		s.top = len(s.i) - 1
+		s.i = s.i[:s.top]
+		return data
+	}
+	fmt.Println("Stack is Empty, no elements to Pop")
+	return nil
+
 }
 
 //IsEmpty cheecks if the stack is empty
 func (s *Stack) IsEmpty() bool {
-	if s.top < 0 {
+	if s.top <= 0 {
 		return true
 	}
 	return false
