aa1e0a5f922a29e5161162e697a06cf6db11cca0
Aakash Banerjee <aakash.banerjee@gmail.com>
2021-04-29 23:53:50 -0700

    stack

--- stack/stack.go
+++ stack/stack.go
@@ -11,3 +11,22 @@ func (s *Stack) Push(i int) {
 	s.i = append(s.i, i)
 	s.top++
 }
+
+//Display Stack
+func (s *Stack) Display() ([]int, int) {
+	return s.i, s.top
+}
+
+//Pop removes the last inserted element from thee stack
+func (s *Stack) Pop() {
+	s.top--
+	s.i = s.i[:s.top]
+}
+
+//IsEmpty cheecks if the stack is empty
+func (s *Stack) IsEmpty() bool {
+	if s.top < 0 {
+		return true
+	}
+	return false
+}
